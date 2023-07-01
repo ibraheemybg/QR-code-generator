8d84f3bc8f3ff5700a6cda8b26310d5c135cf329
Ibraheem Ybg <ibrahimyabagi9@gmail.com>
2023-07-01 11:28:46 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+        message: "Enter URL here: ",
+        name: "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-img.png"));
+
+    fs.writeFile("URL.text", url, (err) => {
+        if(err) {
+            console.log(err);
+        };
+
+        console.log("QR created and saved successfully");
+    })
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
